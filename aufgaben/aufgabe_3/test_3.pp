fa069f4c8a87b77362b366cef943ea562c666bda
Cpp-dino <106454221+Cpp-dino@users.noreply.github.com>
2025-11-05 00:26:51 +0100

    Test-Quelltext 3 hochgeladen

---
+++ aufgaben/aufgabe_3/test_3.pp
@@ -0,0 +1,28 @@
+
+a
+  :=
+  2  # Variable a (Wert: 2)
+b
+  := # Variable a (Wert: 2)
+  3   
+
+if                              # komplexe Bedingung folgt:
+ 2*1  -1< 1 
+  * 14/    2==    1
+    < 22 
++
+        a   -
+
+91 
+            *
+ 7/b
+do
+                                # Zuweisung (falls Bedingung zutrifft)
+        c    
+ :=           "wuppie"
+        else                    # ansonten:
+  do
+c:=
+  
+    "nope"                      # "nope" als Wert für neue Variable c
+        end
