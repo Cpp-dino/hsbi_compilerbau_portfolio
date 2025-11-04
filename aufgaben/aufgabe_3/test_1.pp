14a583cb3f39b6507b057959eef90b7b5ca79806
Cpp-dino <106454221+Cpp-dino@users.noreply.github.com>
2025-11-05 00:26:03 +0100

    Test-Quelltext 1 hochgelden

---
+++ aufgaben/aufgabe_3/test_1.pp
@@ -0,0 +1,40 @@
+
+# Zuweisungen
+a
+
+  :=  "abc"
+b       :=     42                   # b ist zunächst 42
+
+# Kontrollstruktur
+if
+
+a == "abc"
+do
+  if   b + 3<  5*5  +  4* 6
+  do                                # Punkt vor Strich beachten!
+
+        b:=     "wuppie"
+
+else
+
+    do
+a:="nope"                           # a auf "nope" setzten
+        end
+  else   do
+b   :=
+
+       7                            # sonst: b auf 7 setzten
+end
+# Schleife
+while 
+    b+ 4<2 *b               do
+  b   :=b     -1                    # b dekrementieren
+
+    if b
+       / 2
+    ==    b+     10
+  do                                # in dem Fall: b neu setzten
+   b:=   b /
+       10  -    1
+end
+    end
